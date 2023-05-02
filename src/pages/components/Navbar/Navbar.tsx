b6092cb09bf378121870feb3d5984ffc9145891b
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-05-02 19:10:18 -0300

    Added Header's brand-icon and name.

--- src/pages/components/Navbar/Navbar.tsx
+++ src/pages/components/Navbar/Navbar.tsx
@@ -1,7 +1,27 @@
+import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 
 const Navbar:React.FC = () => {
-  return <div>Navbar</div>
+  return (
+    <Flex bg='white' height='44px' padding='6px 12px'>
+      <Flex align='center'>
+        <Image
+          src='/images/redditFace.svg'
+          height='30px'
+          alt=''
+        />
+        <Image 
+          src='/images/redditText.svg'
+          height='46px'
+          display={{ base: 'none', md: 'unset' }}
+          alt='Reddit'
+        />
+      </Flex>
+      {/* <Directory /> */}
+      {/* <SearchInput /> */}
+      {/* <RightContent /> */}
+    </Flex>
+  )
 }
 
 export default Navbar;
