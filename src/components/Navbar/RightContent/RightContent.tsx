11bd635273012c6d655ef736aa372413dc0f2e43
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-05-04 18:40:38 -0300

    Added unstyled Auth Buttons

---
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/react';
+import AuthButtons from './AuthButtons';
+
+type RightContentProps = {
+  // user
+};
+
+const RightContent:React.FC<RightContentProps> = () => {
+  
+  return (
+    <>
+      {/* <AuthModal /> */}
+      <Flex justify='center' align='center'>
+        <AuthButtons />
+      </Flex>
+    </>
+  )
+}
+export default RightContent;
