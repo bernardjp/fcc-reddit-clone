339352175fddc271c4b258cc2afc687d77b962ab
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-05-04 18:40:38 -0300

    Added unstyled Auth Buttons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Flex, Image } from '@chakra-ui/react';
 import SearchInput from './SearchInput';
+import RightContent from './RightContent/RightContent';
 
 const Navbar:React.FC = () => {
   return (
@@ -20,7 +21,7 @@ const Navbar:React.FC = () => {
       </Flex>
       <SearchInput />
       {/* <Directory /> */}
-      {/* <RightContent /> */}
+      <RightContent />
     </Flex>
   )
 }
---
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+
+const AuthButtons:React.FC = () => {
+  return (
+    <>
+      <Button>Log In</Button>
+      <Button>Sign Up</Button>
+    </>
+  )
+}
+export default AuthButtons;
