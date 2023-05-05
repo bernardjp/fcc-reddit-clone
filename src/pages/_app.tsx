f26c98f9a9f464665fcfd8009039ba1d3eb051d3
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-05-05 15:56:04 -0300

    Added Recoil Provider to the root app, and added an Atom to handle modal state.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 import { StyleProviders } from '../chakra/ChakraCustomProvider';
 import Layout from '../components/Layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <StyleProviders>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </StyleProviders>
+    <RecoilRoot>
+      <StyleProviders>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </StyleProviders>
+    </RecoilRoot>
   );
 }
