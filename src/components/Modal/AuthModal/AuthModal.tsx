d0afd6f171fd6605ba919e25afda34e87d77e00a
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-05-05 10:13:36 -0300

    Added basic Modal Component (without functionality).

---
+++ src/components/Modal/AuthModal/AuthModal.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
+
+const AuthModal:React.FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      {/*To be replaced with AuthButtons*/}
+      {/* <Button onClick={onOpen}>Open Modal</Button> */}
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Here is the modal Body</ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default AuthModal;
