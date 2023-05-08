efdaef89647d9f4cb9bb83402e48123ce8bc42ea
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-05-08 15:41:59 -0300

    Added FIrebase Sign-up authentication with Google provider.

--- src/components/Modal/AuthModal/OAuthButtons.tsx
+++ src/components/Modal/AuthModal/OAuthButtons.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Flex, Image, Button } from '@chakra-ui/react';
+import { Flex, Image, Button, Text } from '@chakra-ui/react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb={2}>
+      <Button
+        variant="oauth"
+        mb={2}
+        onClick={() => signInWithGoogle()}
+        isLoading={loading}
+      >
         <Image src="/images/googlelogo.png" alt="logo" height="20px" mr={4} />
         Continue with Google
       </Button>
-      <Button variant="oauth" mb={2}>
-        Other Providers
-      </Button>
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
