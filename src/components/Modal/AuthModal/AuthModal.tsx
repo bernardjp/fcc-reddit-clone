483bbe83d93fecb54ffc5912b36f8ea9f6160ff1
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-05-09 12:46:36 -0300

    Added Logout button and functionality. Added login behavior to Auth Modal.

--- src/components/Modal/AuthModal/AuthModal.tsx
+++ src/components/Modal/AuthModal/AuthModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { authModalState } from '@/atoms/authModalAtom';
 import {
   Flex,
@@ -13,16 +13,24 @@ import {
 import { useRecoilState } from 'recoil';
 import AuthInputs from './AuthInputs';
 import OAuthButtons from './OAuthButtons';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+  const [user, loading, error] = useAuthState(auth);
 
-  const handleCloseModal = () => {
+  const handleCloseModal = useCallback(() => {
     setModalState((prev) => ({
       ...prev,
       open: false,
     }));
-  };
+  }, [setModalState]);
+
+  useEffect(() => {
+    if (user) handleCloseModal();
+    console.log(user);
+  }, [user, handleCloseModal]);
 
   return (
     <>
